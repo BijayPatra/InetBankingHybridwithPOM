f7a59dd9390918557e975f04e4d9bec08da82523
BijayPatra <vijaypatra2681@gmail.com>
2024-10-24 00:28:34 +0530

    modified

--- src/test/java/com/inetbanking/testCases/TC_LoginDDT_002.java
+++ src/test/java/com/inetbanking/testCases/TC_LoginDDT_002.java
@@ -56,8 +56,6 @@ public class TC_LoginDDT_002 extends BaseClass
 		}
 		
 	}
-	
-	
 	@DataProvider(name="LoginData")
 	String [][] getData() throws IOException
 	{
@@ -67,16 +65,14 @@ public class TC_LoginDDT_002 extends BaseClass
 		int colcount=XLUtils.getCellCount(path,"Sheet1",1);
 		
 		String logindata[][]=new String[rownum][colcount];
-		
 		for(int i=1;i<=rownum;i++)
 		{
 			for(int j=0;j<colcount;j++)
 			{
 				logindata[i-1][j]=XLUtils.getCellData(path,"Sheet1", i,j);//1 0
 			}
-				
 		}
-	return logindata;
+		return logindata;
 	}
 	
 }
